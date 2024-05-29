02ca9cff6e6ad46c04e7a03fa233da9cd43ec0cb
Steve Schwab <schwabs@umich.edu>
2024-05-29 12:18:46 -0700

    `checkAccountPayload` returns a status 400 with if `req.body` is invalid

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,7 +1,23 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  const { name, budget } = req.body;
+
+  if (name === undefined || budget === undefined) {
+    return next({ status: 400, message: "name and budget are required" })
+  }
+  
+  if (name.trim().length < 3 || name.trim().length > 100) {
+    return next({ status: 400, message: "name of account must be between 3 and 100" })
+  }
+  
+  if (isNaN(budget)) {
+    return next({ status: 400, message: "budget of account must be a number" })
+  }
+  
+  if (budget < 0 || budget > 1000000) {
+    return next({ status: 400, message: "budget of account is too large or too small" })
+  }
+
+  next();
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
